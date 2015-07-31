260751f828bb0c8dc391fdb1f1f5451621c4d9d7
David Ryder <davidryder@DavidiMacw.home>
2015-07-31 15:31:04 -0700

    3

--- firmware/scheduler.cpp
+++ firmware/scheduler.cpp
@@ -1,2 +1,127 @@
 #include "scheduler.h"
 
+Scheduler::Scheduler() {
+	// TODO Auto-generated constructor stub
+    reset();
+    _nItems = 0;
+}
+
+Scheduler::~Scheduler() {
+	// TODO Auto-generated destructor stub
+}
+
+void Scheduler::reset() {
+	for (int i=0; i < MAX_ITEMS; i++) {
+		taskList[i].h = 0;
+	}
+}
+
+void Scheduler::print() {
+	for (int i=0; i < MAX_ITEMS; i++) {
+		Item ti = (Item)taskList[i];
+		if ( ti.h != 0) {
+			//cout << ti.name<< " " << ti.start << endl;
+		}
+	}
+}
+
+
+void Scheduler::create( char *  name, Handler h, unsigned long startSecs ) {
+	// run once in number of seconds
+	this->createTask( name, h, FIXED, timeFuture( startSecs ), 0, 9999, 1 );
+}
+
+void Scheduler::create( char *  name, Handler h, unsigned long start, unsigned long offset, unsigned long interval) {
+	// run idefintely first at start time and at set intervals
+	this->createTask( name, h, REPEAT, timeRoundUp( start, offset), offset, interval, 0 );
+}
+
+void Scheduler::create( char *  name, Handler h, unsigned long start, unsigned long offset, unsigned long interval, unsigned long n ) {
+	// repeat fixed number of times
+	this->createTask( name, h, FIXED, timeRoundUp( start, offset ), offset, interval, n );
+}
+
+
+void Scheduler::createTask( char *  name, Handler h, TaskType type, unsigned long start, unsigned long offset, unsigned long interval, unsigned long n ) {
+	for (int i=0; i < MAX_ITEMS; i++) {
+		Item *ti = &taskList[i];
+		if ( ti->h == 0) {
+			ti->name = name;
+			ti->h = h;
+			ti->type = type;
+			ti->start = start ; // number of seconds to start
+			ti->offset = offset;
+			ti->interval = interval; // every xx seconds
+			ti->n = n;
+			ti->iterations = 0;
+			break;
+		}
+	}
+}
+
+void Scheduler::remove(  char *  name ) {
+	for (int i=0; i < MAX_ITEMS; i++) {
+		if ( taskList[i].name == name) {
+			taskList[i].h = 0;
+			taskList[i].start = 0;
+			taskList[i].name = '\0';
+			break;
+		}
+	}
+}
+
+unsigned long Scheduler::timeFuture(unsigned long iSec) {
+	//unsigned long now = 9999; // Time.now();
+	//time_t now = time (NULL);
+	unsigned long now = Time.now();
+	return  now + iSec;
+}
+
+unsigned long Scheduler::timeRoundUp(unsigned long iSec, unsigned long offset) {
+  // Round now up / future to next iSec period exactly - add offset to adjust
+  //unsigned long now = 9999; // Time.now();
+  //time_t now = time (NULL);
+  unsigned long now = Time.now();
+  unsigned long o1 = now % iSec; // how far past
+  return now - o1 + iSec + offset;
+}
+
+unsigned long Scheduler::nextTask(unsigned long maxTime ) {
+	unsigned long nextTask = timeFuture( maxTime ); // maximum time to next task
+	//time_t now = time (NULL);
+    unsigned long now = Time.now();
+	for (int i=0; i < MAX_ITEMS; i++) {
+		Item *ti = &taskList[i];
+		if ( ( ti->h != 0) and (ti->start > 0) ) {
+			if ( ti->start < nextTask ) {
+				nextTask = ti->start;
+			}
+		}
+	}
+	return abs( (long)(nextTask - now) ); // number of seconds until next task, may have past, or 0
+}
+
+
+void Scheduler::execute() {
+	//time_t now = time (NULL);
+	unsigned long now = Time.now();
+	for (int i=0; i < MAX_ITEMS; i++) {
+		Item *ti = &taskList[i];
+		if ( ( ti->h != 0) and (ti->start > 0) ) {
+			if ( now > ti->start ) {
+				//cout << "E " << now << " ";
+				ti->h(); // run the task
+				ti->iterations++;
+				ti->start = timeRoundUp( ti->interval, ti->offset); // reschedule
+
+				// Handle fixed number of times
+				if ( ti->type == FIXED ) {
+					ti->n--;
+					if ( ti->n < 1 ) {
+						remove( ti->name ); // stop and remove
+					}
+				}
+			}
+		} // if
+	} // for
+}
---
+++ firmware/scheduler.h
@@ -0,0 +1,62 @@
+/*
+ * scheduler.h
+ *
+ *  Created on: Jul 19, 2015
+ *      Author: davidryder
+ */
+
+#ifndef SCHEDULER_H_
+#define SCHEDULER_H_
+
+#include <string>
+//using namespace std;
+
+#define MAX_ITEMS     4
+#define MAX_NAME_LEN 16
+
+enum TaskType {
+    REPEAT = 1,
+    FIXED = 2
+};
+
+
+
+typedef void (* Handler)();
+
+typedef struct {
+    char *  name;
+    Handler h;
+    TaskType type;
+    unsigned long start;
+    unsigned long interval;
+    unsigned long offset;
+    unsigned long n;
+    unsigned long iterations;
+} Item;
+
+
+class Scheduler
+{
+public:
+    Scheduler();
+    void reset();
+    void print();
+    void create( char *  name, Handler h, unsigned long start );
+    void create( char *  name, Handler h, unsigned long start, unsigned long offset, unsigned long interval );
+    void create( char *  name, Handler h, unsigned long start, unsigned long offset, unsigned long interval, unsigned long n );
+    void remove( char *  name );
+    void execute();
+    unsigned long nextTask(unsigned long maxTime );
+    unsigned long timeFuture(unsigned long iSec);
+    unsigned long timeRoundUp(unsigned long iSec, unsigned long offset);
+    TaskType type;
+	virtual ~Scheduler();
+
+private:
+    void createTask( char *  name, Handler h, TaskType type, unsigned long start, unsigned long offset, unsigned long interval, unsigned long n );
+	Item taskList[ MAX_ITEMS ];
+    int _nItems;
+};
+
+
+#endif /* SCHEDULER_H_ */
